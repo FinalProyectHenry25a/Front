198301f2bf4713878850613867010386ee3ddd9e
MaxiBlanc <maxiblanc240801@gmail.com>
2022-06-20 20:17:51 -0300

    register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
           </Routes>
       </BrowserRouter>
   );
